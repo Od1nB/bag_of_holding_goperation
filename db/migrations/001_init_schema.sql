40bb1294111c90b7fe4010b78f5b50e8c775de9e
Odin Bjørnebo <odinbjoernebo@gmail.com>
2025-11-01 08:58:25 +0100

    init: added db migrations

--- db/migrations/001_init_schema.sql
+++ db/migrations/001_init_schema.sql
@@ -1,4 +1,14 @@
+CREATE DATABASE bagofholding;
 
 CREATE TABLE products (
-    id PRIMARY KEY
-)
+    id          SERIAL       PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    amount      INT          NOT NULL DEFAULT 0,
+    price       NUMERIC      NOT NULL DEFAULT 0.00
+);
+
+INSERT INTO products
+    (name, description, amount, price)
+VALUES
+    ('shortsword', 'pointy sword that deals piercing 1D6 damage on hit', 13, 10);
---
+++ db/migrations/010_seed_data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO products
+--     (name, description, amount, price)
+-- VALUES
+--     ('shortsword',
+--      'pointy sword that deals piercing 1D6 damage on hit',
+--      13,
+--      10
+--     );
